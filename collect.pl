a9074810fdb9d4d0fcbfc172833cbaa9655b96fc
ericlyon <audiodidact@gmail.com>
2023-10-13 20:02:33 -0400

    updated collect.pl

--- collect.pl
+++ collect.pl
@@ -3,7 +3,10 @@ if (! -e "lyonpotpourri"){
 }
 while(<*>){
     chomp;
-    if(/darwin$/ | /liblyonpotpourri.dylib/){
+    if(/darwin$/ | /liblyonpotpourri.dylib/ | /help.pd/){
 	`mv $_ lyonpotpourri`;
     }
 }
+`cp lpp-icon.pd lyonpotpourri`;
+`cp lpp-meters.pd lyonpotpourri`;
+`mv sound lyonpotpourri`;
